3995c0c01285b4be4ff774faa703f6fdb4233850
AnushaG2201 <76487202+AnushaG2201@users.noreply.github.com>
2021-01-16 16:59:34 +0530

    [Anusha] Update Readme.md with problem and solution.

---
+++ src/main/java/org/oop/Points.java
@@ -0,0 +1,23 @@
+package org.oop;
+
+public class Points {
+    private double x;
+    private double y;
+
+    public Points(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Points from, Points to) {
+        double xDistance = to.x - from.x;
+        double yDistance = to.y - from.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public static double direction(Points from, Points to) {
+        double xDistance = to.x - from.x;
+        double yDistance = to.y - from.y;
+        return Math.atan2(yDistance, xDistance);
+    }
+}
